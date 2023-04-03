372b00c433ceedd54deab4816a482dca3eef0705
Milosz <miloszloniewski02@gmail.com>
2023-04-03 16:01:56 +0200

    finished challenge

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+const subscribeBtn = document.querySelector('.newsletter__btn');
+const emailInput = document.querySelector('.newsletter__email');
+
+const showError = (input, msg) => {
+	const formBox = input.parentElement;
+	formBox.classList.add('error');
+	const errorInfo = document.querySelector('.newsletter__error');
+	errorInfo.textContent = msg;
+};
+
+const clearError = input => {
+	const formBox = input.parentElement;
+	formBox.classList.remove('error');
+};
+
+const checkMail = input => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+
+	if (re.test(input.value)) {
+		clearError(input);
+	} else if (input.value === '') {
+		showError(input, `Can't be blank`);
+	} else {
+		showError(input, `Ooops! That doesn't look like an email addres`);
+	}
+};
+
+subscribeBtn.addEventListener('click', () => {
+	checkMail(emailInput);
+});
